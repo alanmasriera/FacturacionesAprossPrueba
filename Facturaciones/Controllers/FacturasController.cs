dffa099709d717cb6ac44ddd218386d9acddaac5
Alan Masriera PC <alan.masriera@gmail.com>
2022-05-17 10:42:17 -0300

    Primer commit

---
+++ Facturaciones/Controllers/FacturasController.cs
@@ -0,0 +1,33 @@
+﻿using Facturaciones.DAL;
+using Facturaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using MongoDbAPI.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Facturaciones.Controllers
+{
+    public class FacturasController : Controller
+    {
+        FacturasRepository repo = new FacturasRepository();
+        //ItemFacturableCollection repo2 = new ItemFacturableCollection();
+        // GET: Facturas
+        public async Task<ActionResult> Index()
+        {
+            //IEnumerable<Facturas> lista = repo.GetFacturasPorPrestador("Alan Masriera");
+            //return View(lista);
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://localhost:7110/api/itemFacturable");
+            List<ItemFacturable> itemsFacturables = JsonConvert.DeserializeObject<List<ItemFacturable>>(json);
+            return View(itemsFacturables);
+        }
+
+        // GET: Facturas/Details/5
+
+    }
+}
---
+++ Facturaciones/DAL/FacturasRepository.cs
@@ -0,0 +1,28 @@
+﻿using Facturaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Facturaciones.DAL
+{
+    public class FacturasRepository
+    {
+        public IEnumerable<Facturas> GetFacturasPorPrestador(string nombreAutor)
+        {
+            var facturas = new Facturas();
+            using(FacturasEntities2 contexto = new FacturasEntities2())
+            {
+                return contexto.Facturas.Where(f => f.nombre_autor == nombreAutor).ToList();
+            }
+        }
+        public IEnumerable<Facturas> GetFacturas()
+        {
+            var facturas = new Facturas();
+            using (FacturasEntities2 contexto = new FacturasEntities2())
+            {
+                return contexto.Facturas.AsNoTracking().ToList();
+            }
+        }
+    }
+}
